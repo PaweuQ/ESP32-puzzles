08a2ced4f378a6b7c7dca8df5f9c7665a43ddaac
Paweł Bartkiewicz <122686426+PaweuQ@users.noreply.github.com>
2025-06-29 10:23:12 +0200

    Add files via upload

---
+++ lights/2_Traffic-indicator/2_Traffic-indicator.ino
@@ -0,0 +1,39 @@
+//INFORMACJE PODSTAWOWE
+//Program: Sygnalizator swietlny
+//Autor: Paweł Bartkiewicz 
+//mail: 15g.pawel.bart@gmail.com
+
+//INFORMACJE DODATKOWE
+//sekwencja zaczyna sie od zielonego swiatla
+
+//czas trwania kazdego ze swiatel mozna latwo dostosowac
+//zmieniajac opoznienia
+
+const int zielona  = 18;
+const int zolta    = 19;
+const int czerwona = 21;
+
+void setup() {
+  pinMode(zielona, OUTPUT);
+  pinMode(zolta, OUTPUT);
+  pinMode(czerwona, OUTPUT);
+}
+
+void loop() {
+  lights();
+}
+
+void lights(){
+  digitalWrite(zielona, HIGH);
+  delay(5000);
+  digitalWrite(zielona, LOW);
+  digitalWrite(zolta, HIGH);
+  delay(2000);
+  digitalWrite(zolta, LOW);
+  digitalWrite(czerwona, HIGH);
+  delay(5000);
+  digitalWrite(zolta, HIGH);
+  delay(2000);
+  digitalWrite(czerwona, LOW);
+  digitalWrite(zolta, LOW);
+}
