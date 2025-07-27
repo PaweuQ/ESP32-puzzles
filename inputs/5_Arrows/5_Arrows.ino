6f23a917fc85acdb77fa083c9a0d1474e7d7e7b0
Paweł Bartkiewicz <122686426+PaweuQ@users.noreply.github.com>
2025-07-27 19:35:00 +0200

    Update 5_Arrows.ino

--- inputs/5_Arrows/5_Arrows.ino
+++ inputs/5_Arrows/5_Arrows.ino
@@ -11,10 +11,10 @@
 // przyciski inkrementuja i dekrementuja wartosci zmiennych 
 // w dwoch osiach X i Y
 
-#define up    18
-#define down  19
-#define right 21
-#define left  22
+#define up    19
+#define down  18
+#define right 22
+#define left  21
 
 void setup() {
   
